ae1fef1e016d1f06475fc538cb7a19d7e1880c5d
Brian Hogan <brianhogan@napcs.com>
2022-09-15 21:51:34 -0500

    add additional logging to activity file

--- activity.go
+++ activity.go
@@ -2,10 +2,16 @@ package app
 
 import (
 	"context"
+	"log"
 )
 
 // @@@SNIPSTART money-transfer-project-template-go-activity-withdraw
 func Withdraw(ctx context.Context, data PaymentDetails) (string, error) {
+	log.Printf(
+		"Withdrawing $%d from account %s.\n\n",
+		data.Amount,
+		data.SourceAccount,
+	)
 	bank := BankingService{"bank-api.example.com"}
 	confirmation, err := bank.Withdraw(data.SourceAccount, data.Amount)
 	return confirmation, err
@@ -15,6 +21,11 @@ func Withdraw(ctx context.Context, data PaymentDetails) (string, error) {
 
 // @@@SNIPSTART money-transfer-project-template-go-activity-deposit
 func Deposit(ctx context.Context, data PaymentDetails) (string, error) {
+	log.Printf(
+		"Depositing $%d into account %s.\n\n",
+		data.Amount,
+		data.TargetAccount,
+	)
 	bank := BankingService{"bank-api.example.com"}
 	// Uncomment the next line and comment the one after that to simulate failure
 	// confirmation, err := bank.DepositThatFails(data.TargetAccount, data.Amount)
